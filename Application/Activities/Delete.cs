19b9b0915e10c4a8cf47cc038547b888f2117586
Szymon Kopeć <heis125@gmail.com>
2023-08-08 20:44:42 +0200

    End of section 4

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Comand : IRequest 
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Comand>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public DataContext Context { get; }
+
+            public async Task<Unit> Handle(Comand request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                _context.Remove(activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
